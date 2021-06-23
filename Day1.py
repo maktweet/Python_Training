a839e6f4bc948db6ad8e1c0db07386b75ab34b30
Mohammad Ali Khan <maktweet@gmail.com>
2021-06-24 00:53:25 +0600

    added another file and changes in the day1 file

--- Day1.py
+++ Day1.py
@@ -3,3 +3,7 @@
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
+
+print("====================================")
+# Strin in a single line of above text
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
