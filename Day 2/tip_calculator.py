f7cd7ecae58ad9ab8c5b6d6a612baf34615c9400
Mohammad Ali Khan <maktweet@gmail.com>
2021-06-24 03:42:38 +0600

    add comment in the tip calculator

--- Day 2/tip_calculator.py
+++ Day 2/tip_calculator.py
@@ -5,5 +5,6 @@ actual_tip_with_bill = (total_bill*tip_percentage)/100 + total_bill
 number_of_people = int(input("How many people to split the bill? "))
 
 each_person_get = round(actual_tip_with_bill/number_of_people,2)
+# formatting string to show the 2 decmal point {:.2f}
 each_person_get = "{:.2f}".format(each_person_get)
 print(f"Each person should pay {each_person_get}")
